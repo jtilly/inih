4a044d2d65627a8b9987aacba08b9e4b02741cb2
supercoder186 <aadit.kolar@gmail.com>
2020-06-04 17:42:38 +0530

    updated to fopen_s and strncpy_s

--- INIReader.h
+++ INIReader.h
@@ -160,7 +160,7 @@ inline static char* find_chars_or_comment(const char* s, const char* chars)
 /* Version of strncpy that ensures dest (size bytes) is null-terminated. */
 inline static char* strncpy0(char* dest, const char* src, size_t size)
 {
-    strncpy(dest, src, size);
+    strncpy_s(dest, size + 1, src, size);
     dest[size - 1] = '\0';
     return dest;
 }
@@ -289,7 +289,7 @@ inline int ini_parse(const char* filename, ini_handler handler, void* user)
     FILE* file;
     int error;
 
-    file = fopen(filename, "r");
+    fopen_s(&file, filename, "r");
     if (!file)
         return -1;
     error = ini_parse_file(file, handler, user);
